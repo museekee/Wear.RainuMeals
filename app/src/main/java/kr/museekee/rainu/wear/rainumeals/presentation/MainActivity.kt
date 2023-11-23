c440491dcdeafa851276f7d139c74c8f9a0854d7
museekee <103117239+museekee@users.noreply.github.com>
2023-11-24 01:30:07 +0900

    임시로 로터리 입력 만듦.

--- app/src/main/java/kr/museekee/rainu/wear/rainumeals/presentation/MainActivity.kt
+++ app/src/main/java/kr/museekee/rainu/wear/rainumeals/presentation/MainActivity.kt
@@ -7,22 +7,25 @@
 package kr.museekee.rainu.wear.rainumeals.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
-import androidx.compose.foundation.gestures.scrollBy
-import androidx.wear.compose.foundation.lazy.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import com.google.android.horologist.compose.pager.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.res.stringResource
@@ -30,8 +33,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.foundation.ExperimentalWearFoundationApi
 import androidx.wear.compose.foundation.lazy.AutoCenteringParams
-import androidx.wear.compose.material.*
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
+import androidx.wear.compose.material.HorizontalPageIndicator
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.PageIndicatorState
+import androidx.wear.compose.material.PageIndicatorStyle
+import androidx.wear.compose.material.Text
 import kotlinx.coroutines.launch
 import kr.museekee.rainu.wear.rainumeals.R
 import kr.museekee.rainu.wear.rainumeals.presentation.libs.Meals
@@ -48,7 +58,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalWearFoundationApi::class)
 @Composable
 fun WearApp(meals: List<String>) {
     val pagerState = rememberPagerState { meals.size }
@@ -72,26 +82,29 @@ fun WearApp(meals: List<String>) {
     }
 
     RainuMealsTheme {
-        val focusRequester = remember { FocusRequester() }
-        val coroutineScope = rememberCoroutineScope()
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MaterialTheme.colors.background)
-                .padding(6.dp)
-                .onRotaryScrollEvent {
-                    Log.d("aaa", it.verticalScrollPixels.toString())
-                    coroutineScope.launch {
-                        pagerState.scrollBy(it.verticalScrollPixels)
-
-                        pagerState.animateScrollToPage(0)
-                    }
-                    true
-                }
-                .focusRequester(focusRequester)
-                .focusable(),
+                .padding(6.dp),
         ) {
+            val coroutineScope = rememberCoroutineScope()
+            val focusRequester = rememberActiveFocusRequester()
             HorizontalPager(
+                modifier = Modifier
+                    .onRotaryScrollEvent {
+                        coroutineScope.launch {
+
+                            if (it.verticalScrollPixels < 1)
+                                pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                            else
+                                pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                        }
+                        true
+                    }
+                    .focusRequester(focusRequester)
+                    .focusable(),
+                beyondBoundsPageCount = 35,
                 state = pagerState
             ) { page ->
                 val splitMeal = meals[page].split("\n")
@@ -101,6 +114,12 @@ fun WearApp(meals: List<String>) {
                         .fillMaxSize(),
                     autoCentering = AutoCenteringParams(0)
                 ) {
+                    item {
+                        Text(
+                            text = page.toString(),
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                     items(splitMeal.size) {
                         Text(
                             text = splitMeal[it],
